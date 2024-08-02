9584ae85e3b6754b5cc438a7fa2a1900ccef7e41
Maxim Zaykov <8859590+makscraft@users.noreply.github.com>
2024-08-02 13:13:58 +0200

    Add files via upload

---
+++ core/installation.class.php
@@ -0,0 +1,13 @@
+<?
+class Installation
+{
+    public function finish()
+    {
+        echo __FUNCTION__;
+    }
+
+    public function postAutoloadDump()
+    {
+        echo __FUNCTION__;
+    }
+}
