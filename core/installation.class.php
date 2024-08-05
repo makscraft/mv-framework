1b12aa99f716512dbb3c13c5e54f104b14abdb09
Maksim Zaykov <8859590+mszaykov@users.noreply.github.com>
2024-08-05 15:26:12 +0200

    Installation

--- core/installation.class.php
+++ core/installation.class.php
@@ -1,31 +1,121 @@
 <?php
+use Composer\Script\Event;
+
 class Installation
 {
+    //Heplers
+
+    static public function typePrompt(string $message)
+    {
+        echo PHP_EOL.$message.": ";
+
+        $stdin = fopen('php://stdin', 'r');
+        $answer = trim(fgets($stdin));
+        fclose($stdin);
+        
+        return $answer;
+    }
+
+    static public function displayErrorMessage(string $message)
+    {
+        echo PHP_EOL."\033[41m ".$message." \033[0m".PHP_EOL.PHP_EOL;
+    }
+
+    static public function displaySuccessMessage(string $message)
+    {
+        echo PHP_EOL."\033[92m ".$message." \033[0m".PHP_EOL.PHP_EOL;
+    }
+
+    static public function displayDoneMessage(string $message)
+    {
+        echo PHP_EOL."\033[42m ".$message." \033[0m".PHP_EOL.PHP_EOL;
+    }
+
+    static public function setEnvFileParameter(string $key, string $value)
+    {
+
+    }
+
+    //Installation process
+
     static public function finish()
     {
-        $env = __DIR__.'/../.env.empty';
-        $text = file_get_contents($env);
+        $env_file = __DIR__.'/../.env.empty';
+        $env = file_get_contents($env_file);
 
         $value = Service :: strongRandomString(40);
-        $text = str_replace('APP_TOKEN=', 'APP_TOKEN='.$value, $text);
+        $env = str_replace('APP_TOKEN=', 'APP_TOKEN='.$value, $env);
 
-        file_put_contents(__DIR__.'/../.env', $text);
-        echo ".env file has been configurated\r\n";
+        file_put_contents(__DIR__.'/../.env', $env);
+        echo "\033[92m .env file has been configurated\033[0m\r\n";
 
         $file = __DIR__.'/../index.php';
-        $text = file_get_contents($file);
-        $text = str_replace('config/autoload.php', 'vendor/autoload.php', $text);
-        file_put_contents($file, $text);
-        echo "index.php file has been configurated\r\n";
+        $code = file_get_contents($file);
+        $code = str_replace('config/autoload.php', 'vendor/autoload.php', $code);
+
+        file_put_contents($file, $code);
+        echo "\033[92m index.php file has been configurated\033[0m\r\n";
     }
 
-    static public function postAutoloadDump()
+    static public function configureFolder(Event $event)
     {
-        //echo ".env file has been configurated\r\n";
+        $arguments = $event -> getArguments();
+        $folder = $arguments[0] ?? '';
+        $folder = trim($folder);
+
+        if($folder === '')
+        {
+            echo "\033[44mError! You need to pass the project folder name like -- /my/application/ or -- myapp (or simply -- /).\033[0m\r\n";
+            return;
+        }
+        else if(strpos($folder, '.') !== false)
+        {
+            echo "Error! Project folder name may not contain '.' character.\033[0m\r\n";
+            return;
+        }
+        
+        if(!preg_match('/^\//', $folder))
+            $folder = '/'.$folder;
+
+        if(!preg_match('/\/$/', $folder))
+            $folder = $folder.'/';
+
+        $root = realpath(__DIR__.'/../..');
+        $directory = realpath(__DIR__.'/../../'.$folder);
+
+        if(!is_dir($directory))
+        {
+            echo "Error! Project directory does not exist ".$root.$folder.".\033[0m\r\n";
+            return;
+        }
+
+        $env_file = __DIR__.'/../.env';
+        $htaccess_file = __DIR__.'/../.htaccess';
+        $env = file_get_contents($env_file);
+        $htaccess = file_get_contents($htaccess_file);
+
+        $env = preg_replace('/APP_FOLDER=[\/\w]*/', 'APP_FOLDER='.$folder, $env);
+        $htaccess = preg_replace('/RewriteBase\s+\/[\/\w]*/', 'RewriteBase '.$folder, $htaccess);
+
+        file_put_contents($env_file, $env);
+        file_put_contents($htaccess_file, $htaccess);
+
+        echo "\033[92m.env file has been configurated\033[0m\r\n";
+        echo "\033[92m.htaccess file has been configurated\033[0m\r\n";
     }
 
-    static public function configureDatabase()
+    static public function postAutoloadDump(Event $event)
     {
+        $a = self :: typePrompt('Say hello');
+        self :: displayErrorMessage($a);
+    }
+
+    static public function configureDatabase(Event $event)
+    {
+        echo $event -> getName()."\r\n";
+        print_r($event -> getArguments());
+        exit();
+
         $env = __DIR__.'/../.env';
 
         if(!file_exists($env))
@@ -47,7 +137,7 @@ class Installation
         $dump_file = __DIR__.'/../userfiles/database/mysql-dump.sql';
 
         if(true === self :: loadMysqlDump($dump_file, $pdo))
-            echo "database has been configurated\r\n";
+            echo "\033[92mDatabase has been configurated.\033[0m\r\n";
     }
 
     static public function loadMysqlDump(string $dump_file, PDO $pdo)
