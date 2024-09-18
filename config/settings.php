8176b47b7912396ff564206784e937a4d99d6fdf
Maksim Zaykov <8859590+mszaykov@users.noreply.github.com>
2024-09-18 18:24:21 +0200

    internal version 3.1

--- config/settings.php
+++ config/settings.php
@@ -14,7 +14,7 @@ $mvMainSettings = [
 'SupportedRegions' => ['en', 'us', 'ru', 'de'],
 
 //Initial version of MV framework (do not change it).
-'Version' => 3.0,
+'Version' => 3.1,
 
 //Allowed data types for models' fields
 'ModelsDataTypes' => ['bool','int','float','char','url','redirect','email','phone','password','text','enum','parent',
